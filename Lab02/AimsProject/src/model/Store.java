330ab0366d4a211d2f323180d5e6743f485c12d9
NgDoNgoc <ngodoanngoc60@gmail.com>
2024-11-24 15:10:34 +0700

    Add store and store-test

---
+++ Lab02/AimsProject/src/model/Store.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Store {
+    private final static int CAPACITY = 5;
+    private ArrayList<DigitalVideoDisc> itemsInStore;
+
+    public Store (){
+        itemsInStore = new ArrayList<>(CAPACITY);
+    }
+
+    public void addDVD(DigitalVideoDisc dvd){
+        if (itemsInStore.size() < CAPACITY){
+            itemsInStore.add(dvd);
+            System.out.println(dvd.getTitle() + " has been added in the store.");
+        } else {
+            System.out.println("The store is full. Cannot add " + dvd.getTitle());
+        }
+    }
+
+    public void removeDVD(String title){
+        boolean found = false;
+        for (int i = 0; i < itemsInStore.size(); i++){
+            if (itemsInStore.get(i).isMatch(title)){
+                found = true;
+                itemsInStore.remove(i);
+                System.out.println(title + "has been removed.");
+            }
+        }
+
+        if (!found){
+            System.out.println(title + " does not exist.");
+        }
+    }
+}
---
+++ Lab02/AimsProject/src/test/StoreTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import model.DigitalVideoDisc;
+import model.Store;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "Ron Clements", 88, 18.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Inception", "Science Fiction", "Christopher Nolan", 148, 14.95f);
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Frozen", "Animation", "Chris Buck", 102, 22.95f);
+
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        store.addDVD(dvd4);
+        store.addDVD(dvd5);
+
+        DigitalVideoDisc dvd6 = new DigitalVideoDisc("Toy Story", "Animation", "John Lasseter", 81, 19.99f);
+        store.addDVD(dvd6);
+
+        store.removeDVD("Star Wars");
+
+        store.removeDVD("Harry Potter");
+    }
+}
